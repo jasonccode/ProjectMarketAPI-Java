c52ab1a1908d8cb9d5293696709e85fd4a84c83d
Jason C <grousemusic@gmail.com>
2023-11-14 18:41:23 -0500

    Creando clases

--- src/main/java/com/market/projectmarket/persistence/entity/Categoria.java
+++ src/main/java/com/market/projectmarket/persistence/entity/Categoria.java
@@ -5,7 +5,6 @@ import javax.persistence.*;
 @Entity
 @Table(name = "categorias")
 public class Categoria {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_categoria")
