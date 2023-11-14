09788bea9390693e27250f15714690cb6e17593d
Jason C <grousemusic@gmail.com>
2023-11-14 18:30:15 -0500

    Creando clases

--- src/main/java/com/market/projectmarket/persistence/entity/ComprasProducto.java
+++ src/main/java/com/market/projectmarket/persistence/entity/ComprasProducto.java
@@ -1,13 +1,48 @@
 package com.market.projectmarket.persistence.entity;
 
+
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.IdClass;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "compras_productos")
 public class ComprasProducto {
+    @EmbeddedId
+    private ComprasProductoPK id;
+    private Integer cantidad;
+    private Double total;
+    private Boolean estado;
 
+    public ComprasProductoPK getId() {
+        return id;
+    }
 
+    public void setId(ComprasProductoPK id) {
+        this.id = id;
+    }
 
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
 }
