68b1191ce563ed7a45282b1bc8d0d6329000a098
Jason C <grousemusic@gmail.com>
2023-11-16 18:47:19 -0500

    implementando la anotacion @Service

---
+++ src/main/java/com/market/projectmarket/web/controller/ProductController.java
@@ -0,0 +1,38 @@
+package com.market.projectmarket.web.controller;
+
+import com.market.projectmarket.domain.Product;
+import com.market.projectmarket.domain.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    public List<Product> getAll() {
+        return productService.getAll();
+    }
+
+    public Optional<Product> getProduct(int productId) {
+        return productService.getProduct(productId);
+    }
+
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        return productService.getByCategory(categoryId);
+    }
+
+    public Product save(Product product) {
+        return productService.save(product);
+    }
+
+    public boolean delete(int productId) {
+        return productService.delete(productId);
+    }
+
+}
