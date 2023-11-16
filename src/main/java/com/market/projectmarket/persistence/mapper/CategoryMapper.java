1bde8a3be0dca37d867589e9048d8f3028a11dc7
Jason C <grousemusic@gmail.com>
2023-11-15 19:22:27 -0500

    Orientar nuestra API al dominio con MapStruct

---
+++ src/main/java/com/market/projectmarket/persistence/mapper/CategoryMapper.java
@@ -0,0 +1,19 @@
+package com.market.projectmarket.persistence.mapper;
+
+import com.market.projectmarket.domain.Category;
+import com.market.projectmarket.persistence.entity.Categoria;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+    @Mappings({
+            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "descripcion", target = "category"),
+            @Mapping(source = "estado", target = "active"),
+    })
+    Category toCategory(Categoria categoria);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "productos", ignore = true)
+    Categoria toCategoria(Category category);
+}
