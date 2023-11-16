8dbec65b9d6b372df81fe20c1b5bf0e09fb307ee
Jason C <grousemusic@gmail.com>
2023-11-16 18:08:42 -0500

    Inyección de dependencias

--- src/main/java/com/market/projectmarket/persistence/ProductoRepository.java
+++ src/main/java/com/market/projectmarket/persistence/ProductoRepository.java
@@ -5,6 +5,7 @@ import com.market.projectmarket.domain.repository.ProductRepository;
 import com.market.projectmarket.persistence.crud.ProductoCrudRepository;
 import com.market.projectmarket.persistence.entity.Producto;
 import com.market.projectmarket.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +13,9 @@ import java.util.Optional;
 
 @Repository
 public class ProductoRepository implements ProductRepository {
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
+    @Autowired
     private ProductMapper mapper;
 
     @Override
