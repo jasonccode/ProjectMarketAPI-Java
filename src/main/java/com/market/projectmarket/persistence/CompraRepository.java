1fd9f2e598b873a9e7faaa76a2917553eabd08cc
Jason C <grousemusic@gmail.com>
2023-11-17 15:50:01 -0500

    Crear el repositorio de compras

---
+++ src/main/java/com/market/projectmarket/persistence/CompraRepository.java
@@ -0,0 +1,39 @@
+package com.market.projectmarket.persistence;
+
+import com.market.projectmarket.domain.Purchase;
+import com.market.projectmarket.domain.repository.PurchaseRepository;
+import com.market.projectmarket.persistence.crud.CompraCrudRepository;
+import com.market.projectmarket.persistence.entity.Compra;
+import com.market.projectmarket.persistence.mapper.PurchaseMapper;
+import com.sun.jdi.PrimitiveValue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CompraRepository implements PurchaseRepository {
+    @Autowired
+    private CompraCrudRepository compraCrudRepository;
+    @Autowired
+    private PurchaseMapper mapper;
+
+    @Override
+    public List<Purchase> getAll() {
+        return mapper.toPurchases((List<Compra>) compraCrudRepository.findAll());
+    }
+
+    @Override
+    public Optional<List<Purchase>> getByClient(String clientId) {
+        return compraCrudRepository.findByIdCliente(clientId)
+                .map(compras -> mapper.toPurchases(compras));
+    }
+
+    @Override
+    public Purchase save(Purchase purchase) {
+        Compra compra = mapper.toCompra(purchase);
+        compra.getProductos().forEach(producto -> producto.setCompra(compra));
+        return mapper.toPurchase(compraCrudRepository.save(compra));
+    }
+}
---
+++ src/main/java/com/market/projectmarket/persistence/crud/CompraCrudRepository.java
@@ -0,0 +1,12 @@
+package com.market.projectmarket.persistence.crud;
+
+import com.market.projectmarket.persistence.entity.Compra;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CompraCrudRepository extends CrudRepository<Compra, Integer> {
+    Optional<List<Compra>> findByIdCliente(String idCliente);
+}
+
