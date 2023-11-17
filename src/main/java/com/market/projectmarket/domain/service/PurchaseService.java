4c513d227d691a43d34057bbecfa33fb260865dd
Jason C <grousemusic@gmail.com>
2023-11-17 16:06:05 -0500

    Crear servicio y controlador de purchase

---
+++ src/main/java/com/market/projectmarket/domain/service/PurchaseService.java
@@ -0,0 +1,27 @@
+package com.market.projectmarket.domain.service;
+
+import com.market.projectmarket.domain.Purchase;
+import com.market.projectmarket.domain.repository.PurchaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PurchaseService {
+    @Autowired
+    private PurchaseRepository purchaseRepository;
+
+    public List<Purchase> getAll() {
+        return purchaseRepository.getAll();
+    }
+
+    public Optional<List<Purchase>> getByClient(String clientId) {
+        return purchaseRepository.getByClient(clientId);
+    }
+
+    public Purchase save(Purchase purchase) {
+        return purchaseRepository.save(purchase);
+    }
+}
---
+++ src/main/java/com/market/projectmarket/web/controller/PurchaseController.java
@@ -0,0 +1,34 @@
+package com.market.projectmarket.web.controller;
+
+import com.market.projectmarket.domain.Purchase;
+import com.market.projectmarket.domain.service.PurchaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/purchases")
+public class PurchaseController {
+    @Autowired
+    private PurchaseService purchaseService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Purchase>> getAll() {
+        return new ResponseEntity<>(purchaseService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/client/{idClient}")
+    public ResponseEntity<List<Purchase>> getByClient(@PathVariable("idClient") String clientId) {
+        return purchaseService.getByClient(clientId).map(
+                purchases -> new ResponseEntity<>(purchases, HttpStatus.OK)
+        ).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Purchase> save(@RequestBody Purchase purchase) {
+        return new ResponseEntity<>(purchaseService.save(purchase), HttpStatus.CREATED);
+    }
+}
